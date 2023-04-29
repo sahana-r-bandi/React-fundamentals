45980218ccdf7e09e85a919558adebfffee2df0f
Sahana Bandi <sahana.vr165@gmail.com>
2023-04-29 16:21:23 -0500

    react compoent using class compoent

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,12 +3,13 @@ import {React, Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Greet />
+        <Welcome />
       </div>
     );
   }
--- hello-world/src/components/Welcome.js
+++ hello-world/src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-class Welcome extends Componet {
+class Welcome extends Component {
     render() {
         return <h1> Class Component</h1>
     }
