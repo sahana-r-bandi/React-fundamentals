c1c86566d5a075f19291b1d0061636e103d4f8fe
Sahana Bandi <sahana.vr165@gmail.com>
2023-05-06 22:44:58 -0500

    props childern

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,5 @@
 
-import {React, Component} from 'react';
+import { React, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
@@ -7,12 +7,21 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div className="App">
-        <Greet />  {/*  Functional component */}
-        <Welcome /> {/* Class compoent */}
-        <Hello /> {/* Functional component without jsx */}
+        <Greet name="Bruce" heroName="Batman">
+          <p>This is child props</p>
+        </Greet>  {/*  Functional component */}
+        <Greet name="Sanvi" heroName="Wonder girl">
+          <button>Hello worlde</button>
+          <p>This is para</p>
+        </Greet>   {/*  Functional component */}
+        <Greet name="Bandi" heroName="Humble hubby" />  {/*  Functional component */}
+        <Greet name="Priyanka" heroName="wify" /> {/* Functional component */}
+
+        {/* <Welcome /> */} {/* Class compoent */}
+        {/* <Hello /> */} {/* Functional component without jsx */}
       </div>
     );
   }
