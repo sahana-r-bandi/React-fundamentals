5b72e7e059dbb3d9942bc3598b81a40d44d10d79
Sahana Bandi <sahana.vr165@gmail.com>
2023-05-06 20:54:03 -0500

    functional componets without jsx

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,12 +4,15 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Welcome />
+        <Greet />  {/*  Functional component */}
+        <Welcome /> {/* Class compoent */}
+        <Hello /> {/* Functional component without jsx */}
       </div>
     );
   }
